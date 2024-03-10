63e2c45aab55efc57bb0333ebd71bbdba3122ac0
Anna R <anmaiden.js@gmail.com>
2024-03-10 15:18:55 +0300

    add theme, classNames functional, some styles

---
+++ src/theme/ThemeContext.ts
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+
+export interface ThemeContextProps {
+  theme?: Theme;
+  setTheme?: (theme: Theme) => void;
+}
+
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
+
+
+export const ThemeContext = createContext<ThemeContextProps>({})
+
+export const LOCAL_STORAGE_THEME_KEY = 'theme';
